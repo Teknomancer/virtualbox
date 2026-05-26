7a3a5632fae3c0801bb28591c41413a1c7894b99
Vadim Galitsyn <vadim.galitsyn@oracle.com>
2026-05-26 16:37:15 +0000

    Linux: Host and Guest: Downgrade RHEL 9.9 changes from r173989 and r173990 to RHEL 9.8, bugref:10908.
    svn:sync-xref-src-repo-rev: r174001

--- src/VBox/Additions/linux/drm/vbox_drv.h
+++ src/VBox/Additions/linux/drm/vbox_drv.h
@@ -1,4 +1,4 @@
-/* $Id: vbox_drv.h 114175 2026-05-22 09:44:04Z vadim.galitsyn@oracle.com $ */
+/* $Id: vbox_drv.h 114186 2026-05-26 16:37:15Z vadim.galitsyn@oracle.com $ */
 /** @file
  * VirtualBox Additions Linux kernel video driver
  */
@@ -306,7 +306,7 @@ struct vbox_private {
 	} ttm;
 
 	struct mutex hw_mutex; /* protects modeset and accel/vbva accesses */
-#if RTLNX_VER_MIN(6,18,0) || RTLNX_RHEL_RANGE(9,9, 9,99) || RTLNX_RHEL_RANGE(10,2, 10,99)
+#if RTLNX_VER_MIN(6,18,0) || RTLNX_RHEL_RANGE(9,8, 9,99) || RTLNX_RHEL_RANGE(10,2, 10,99)
 	/** Replacement for struct drm_device .struct_mutex. */
 	struct mutex struct_mutex;
 #endif
